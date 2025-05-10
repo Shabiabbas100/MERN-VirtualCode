1d602684da0e4b8c48743a9375132fd188da40c6
Shabi Abbas <shabiabbas100@gmail.com>
2025-05-10 16:57:14 +0530

    updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-stone-950 h-12 flex items-center justify-center text-sm text-white">
+    <footer className="bg-stone-950 h-12 flex items-center justify-center text-sm text-white px-4">
       <p>© 2025 CodeX. All rights reserved. Developed by Shabi Abbas.</p>
     </footer>
   );
--- frontend/src/components/Testimonials.jsx
+++ frontend/src/components/Testimonials.jsx
@@ -8,7 +8,7 @@ const Testimonials = () => {
   return (
     <>
     <Navbar/>
-    <div className="mt-20 tracking-wide mb-12">
+    <div className="mt-20 tracking-wide mb-12 h-full">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20">
         What People are saying
       </h2>
