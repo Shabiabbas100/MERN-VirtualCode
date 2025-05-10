52a591e5bffdbd5031deeb6e084ea154dbefd791
Shabi Abbas <shabiabbas100@gmail.com>
2025-05-10 16:59:09 +0530

    updated

--- frontend/src/components/Testimonials.jsx
+++ frontend/src/components/Testimonials.jsx
@@ -8,7 +8,7 @@ const Testimonials = () => {
   return (
     <>
     <Navbar/>
-    <div className="mt-20 tracking-wide mb-12 h-full">
+    <div className="mt-20 tracking-wide mb-12 min-h-screen">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20">
         What People are saying
       </h2>
